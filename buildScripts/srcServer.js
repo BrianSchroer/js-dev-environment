6f566a42431bdc24d2cc2c084c60c672b1542c56
Brian Schroer <brianschroer@gmail.com>
2016-11-15 12:13:38 -0600

    Added npm scripting

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,4 +1,6 @@
-// Development source server
+/**
+ * Development source server
+ */
 
 const express = require('express');
 const path = require('path');
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,7 @@
+/**
+ * Display message when starting the app in dev mode
+ */
+
+const chalk = require('chalk');
+
+console.log(chalk.green('Starting app in dev mode...'));
