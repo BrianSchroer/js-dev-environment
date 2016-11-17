32567d5337625234209a7f91d3499c5c3b96144d
Brian Schroer <brianschroer@gmail.com>
2016-11-16 19:28:06 -0600

    Added HTTP call logic

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -25,6 +25,15 @@ app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(request, response) {
+    // Fake "database" data:
+    response.json([
+        {'id':1, 'firstName':'Bob', 'lastName':'Smith', 'email':'bob@gmail.com'},
+        {'id':2, 'firstName':'Tammy', 'lastName':'Norton', 'email':'tnorton@gmail.com'},
+        {'id':3, 'firstName':'Tina', 'lastName':'Lee', 'email':'lee.tina@gmail.com'}
+    ]);
+});
+
 app.listen(scriptsConfig.port, function(err) {
     if (err) {
         console.log(err);
